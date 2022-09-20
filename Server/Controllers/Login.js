936ab85099087872509a4de591287b74d910792d
vetrivelj1203 <vetriveljaganathan5125@gmail.com>
2022-09-20 12:32:04 +0530

    login completed -vetrivel

---
+++ Server/Controllers/Login.js
@@ -0,0 +1,18 @@
+import { userModel } from "../Schema/userSchema.js";
+import bcrypt from "bcrypt";
+
+export const login = async (req, res) => {
+  const body = req.body;
+  const user = await userModel.findOne({ userEmail: body.userEmail });
+  if (user) {
+    const validPassword = await bcrypt.compare(
+      body.userPassword,
+      user.userPassword
+    );
+    if (validPassword) {
+      res.status(200).send({ message: "Valid password" });
+    } else {
+      res.status(400).send({ error: "Invalid Password" });
+  } 
+  }
+};
--- Server/Routes/Routes.js
+++ Server/Routes/Routes.js
@@ -1,7 +1,9 @@
 import express from "express";
+import { login } from "../Controllers/Login.js";
 import { signup } from "../Controllers/Signup.js";
 
 const Route = express.Router();
 Route.post("/signup", signup);
+Route.get('/login',login)
 
 export default Route;
