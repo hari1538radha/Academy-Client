07e0a3834cd8195e3aa31d794cac8c85b3b129ab
kavinkumar <kavinkumars218@gmail.com>
2022-09-21 18:56:35 +0530

    Created post call and get for landing page

--- Server/Routes/Routes.js
+++ Server/Routes/Routes.js
@@ -1,11 +1,17 @@
 import express from "express";
 import { login } from "../Controllers/Login.js";
 import { signup } from "../Controllers/Signup.js";
-import { uploadData } from "../Controllers/topics.js";
+import { uploadData } from "../Controllers/Topics.js";
+import { eventData } from "../Controllers/Events.js";
+import { topics } from "../Controllers/landingTopic.js";
+import { events } from "../Controllers/landingEvents.js";
 
 const Route = express.Router();
 Route.post("/signup", signup);
 Route.get('/login', login);
 Route.post("/topicUpload", uploadData);
+Route.post("/topEventsUpload", eventData);
+Route.get("/topics", topics);
+Route.get("/events", events);
 
 export default Route;
