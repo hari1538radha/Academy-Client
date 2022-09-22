09035984479343cad9702b1bf129006d8f7b1d7c
aswin2062 <aswincharlie8877@gmail.com>
2022-09-22 09:15:37 +0530

    [Aswin]-landing page css,js,store,are done working in slice

--- Client/src/component/Landing/Footer/footer.js
+++ Client/src/component/Landing/Footer/footer.js
@@ -1,77 +1,71 @@
-import React from 'react'
-import "../Css/footer.css"
-import Linepic from "../Img/Linepic.svg"
-import Logo from "../Img/Academy.svg"
-import Location from '../Img/Locationlogo.svg'
-import callerlogo from '../Img/CallerLogo.svg'
-import printerlogo from '../Img/printerlogo.svg'
-import facebook from '../Img/Facebook.svg'
-import twitter from '../Img/Twitter.svg'
-import Linkedin from '../Img/LinkedIn.svg'
-import youtube from '../Img/Youtube.svg'
-import instagram from '../Img/Instagram.svg'
-import google from '../Img/GooglePlus.svg'
-import pinster from '../Img/Pinterest.svg'
-import Wifilogo from '../Img/wifilogo.svg'
-
+import React from "react";
+import "../Css/footer.css";
+import Linepic from "../Img/Linepic.svg";
+import Logo from "../Img/Academy.svg";
+import Location from "../Img/Locationlogo.svg";
+import callerlogo from "../Img/CallerLogo.svg";
+import printerlogo from "../Img/printerlogo.svg";
+import facebook from "../Img/Facebook.svg";
+import twitter from "../Img/Twitter.svg";
+import Linkedin from "../Img/LinkedIn.svg";
+import youtube from "../Img/Youtube.svg";
+import instagram from "../Img/Instagram.svg";
+import google from "../Img/GooglePlus.svg";
+import pinster from "../Img/Pinterest.svg";
+import Wifilogo from "../Img/wifilogo.svg";
 
 const footer = () => {
-    return (
-        <div className='footer'>
-            <div className='Linepic'><img src={Linepic}></img></div>
+  return (
+    <div className="footer">
+      <div className="Linepic">
+        <img src={Linepic}></img>
+      </div>
 
-            <img className='Logo' src={Logo}></img>
-            <div>
-                <div className='Location-footer'> <img src={Location}></img>
-                    <p>345 Faulconer Drive, Suite 4 • Charlottesville</p></div>
-                <div className='Caller-container'>
-                    <img src={callerlogo}></img>
-                    <p>(123) 456-7</p></div>
-                <div className='printer-container'>
-                    <img src={printerlogo}></img>
-                    <p>(123) 456-7</p></div>
-                <div className='Social'>
-                    <p>
-                        Social Media
-                    </p>
-                    <div className='Social-logos'>
-
-                        <img src={facebook} alt="facebook"></img>
-                        <img src={twitter} alt="twitter"></img>
-                        <img src={Linkedin} alt="Linkedin"></img>
-                        <img src={youtube} alt="Youtube"></img>
-                        <img src={instagram} alt="Instagram"></img>
-                        <img src={google} alt="Google"></img>
-                        <img src={pinster} alt="Pinster"></img>
-                        <img src={Wifilogo} alt="wifilogo"></img>
-                    </div>
-
-                </div>
-
-
-            </div>
-            <div className='Login-btn-footer'>
-                Log in
-            </div>
-            <div className="Line2">
-
-            </div>
-            <div className='footer-link-container'>
-
-                <div className='Footer-link'>
-                    <a href='#'>ABOUT US</a>
-                    <a href='#'> CONTACT US </a>
-                    <a href='#'>HELP</a>
-                    <a href='#'>PRIVACY POLICY</a>
-                    <a href='#'> DISCLAIMER</a>
-                </div>
-                <div className='Copyright-container'>
-                    <p>Copyright © 2018 • Lift Media Inc.</p>
-                </div>
-            </div>
-
+      <img className="Logo" src={Logo}></img>
+      <div>
+        <div className="Location-footer">
+          {" "}
+          <img src={Location}></img>
+          <p>345 Faulconer Drive, Suite 4 • Charlottesville</p>
         </div>
-    )
-}
+        <div className="Caller-container">
+          <img src={callerlogo}></img>
+          <p>(123) 456-7</p>
+        </div>
+        <div className="printer-container">
+          <img src={printerlogo}></img>
+          <p>(123) 456-7</p>
+        </div>
+        <div className="Social">
+          <p>Social Media</p>
+          <div className="Social-logos">
+            <img src={facebook} alt="facebook"></img>
+            <img src={twitter} alt="twitter"></img>
+            <img src={Linkedin} alt="Linkedin"></img>
+            <img src={youtube} alt="Youtube"></img>
+            <img src={instagram} alt="Instagram"></img>
+            <img src={google} alt="Google"></img>
+            <img src={pinster} alt="Pinster"></img>
+            <img src={Wifilogo} alt="wifilogo"></img>
+          </div>
+        </div>
+      </div>
+      <div className="Login-btn-footer">Log in</div>
+      <div className="Line2"></div>
+      <div className="footer-link-container">
+        <div className="Footer-link">
+          <a href="#">ABOUT US</a>
+          <a href="#"> CONTACT US </a>
+          <a href="#">HELP</a>
+          <a href="#">PRIVACY POLICY</a>
+          <a href="#"> DISCLAIMER</a>
+        </div>
+        <div className="Copyright-container">
+          <p>Copyright © 2018 • Lift Media Inc.</p>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default footer
+export default footer;
--- Client/src/component/Landing/Landing.js
+++ Client/src/component/Landing/Landing.js
@@ -1,10 +1,11 @@
 import React from "react";
 import NavBar from "./navBar";
 import img1 from "./Img/Frame.svg";
-// import img2 from "./Img/button.svg";
-// import img3 from "./Img/Vector.svg";
+import img2 from "./Img/button.svg";
+import img3 from "./Img/Vector.svg";
 import img4 from "./Img/Rectangle 14.jpg";
-import Footer from "./Footer/footer.js"
+import Footer from "./Footer/footer.js";
+import "./Css/Landing.css";
 
 function landing() {
   return (
@@ -13,8 +14,17 @@ function landing() {
       <>
         <div className="first-container">
           <div className="left">
-            <p>Be led by the dreams in your heart</p>
-            <p>
+            <div className="fst-con-head">
+              <h1>
+                Be led by the <span className="dreams-color">dreams</span>{" "}
+              </h1>
+
+              <h1>
+                in your <span className="heart-color">heart</span>
+              </h1>
+            </div>
+
+            <p className="first-con">
               Success is not how high you have climbed, but how <br></br>you
               make a positive difference to the world
             </p>
@@ -25,75 +35,151 @@ function landing() {
           </div>
         </div>
         <div className="search">
-        
+          <input
+            type="text"
+            placeholder="  Search  a words you prefer"
+            className="search-box"
+            required={true}
+          ></input>
+          <div className="search-img">
+            <img src={img3}></img>
+          </div>
         </div>
-        <div className="second-container">
-          <h1>Topics</h1>
-          <div className="second-container-sub">
-            <div>
-             
-              <a href="">know more</a>
-            </div>
-            <div>
-           
-              <a href="">know more</a>
-            </div>
-            <div>
-            
-              <a href="">know more</a>
-            </div>
-            <div>
-           
-              <a href="">know more</a>
+        <div className="second-contant">
+          <div className="sub-con">
+            <h1 className="topic"> Topics</h1>
+            <div className="sub-top">
+              <div className="sub-top-1">
+                <h2 className="topic1">Education</h2>
+                <p className="sub-contain">
+                  {" "}
+                  Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall.
+                  Gåsade döloning. Syll bebel <br></br>till kvasisens. Tres kos
+                  masade <br></br>robotdräkt. Heteropp. Vönäna teraskap.
+                  <br></br> Vande legga kadinera makrohet <br></br>reatologi.
+                  Por hybridkrig och hypobun.<br></br> Evöligt färade, därför
+                  att påns tilig.{" "}
+                </p>
+                <a href="">
+                  <span class="material-symbols-outlined">menu_book</span>Read
+                  More
+                </a>
+              </div>
+              <div className="sub-top-1">
+                <h2 className="topic1">Education</h2>
+                <p className="sub-contain">
+                  {" "}
+                  Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall.
+                  Gåsade döloning. Syll bebel <br></br>till kvasisens. Tres kos
+                  masade <br></br>robotdräkt. Heteropp. Vönäna teraskap.
+                  <br></br> Vande legga kadinera makrohet <br></br>reatologi.
+                  Por hybridkrig och hypobun.<br></br> Evöligt färade, därför
+                  att påns tilig.{" "}
+                </p>
+                <a href="">
+                  <span class="material-symbols-outlined">menu_book</span>Read
+                  More
+                </a>
+              </div>{" "}
+              <div className="sub-top-1">
+                <h2 className="topic1">Education</h2>
+                <p className="sub-contain">
+                  {" "}
+                  Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall.
+                  Gåsade döloning. Syll bebel <br></br>till kvasisens. Tres kos
+                  masade <br></br>robotdräkt. Heteropp. Vönäna teraskap.
+                  <br></br> Vande legga kadinera makrohet <br></br>reatologi.
+                  Por hybridkrig och hypobun.<br></br> Evöligt färade, därför
+                  att påns tilig.{" "}
+                </p>
+                <a href="">
+                  <span class="material-symbols-outlined">menu_book</span>Read
+                  More
+                </a>
+              </div>{" "}
+              <div className="sub-top-1">
+                <h2 className="topic1">Education</h2>
+                <p className="sub-contain">
+                  {" "}
+                  Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall.
+                  Gåsade döloning. Syll bebel <br></br>till kvasisens. Tres kos
+                  masade <br></br>robotdräkt. Heteropp. Vönäna teraskap.
+                  <br></br> Vande legga kadinera makrohet <br></br>reatologi.
+                  Por hybridkrig och hypobun.<br></br> Evöligt färade, därför
+                  att påns tilig.{" "}
+                </p>
+                <a href="">
+                  <span class="material-symbols-outlined">menu_book</span>Read
+                  More
+                </a>
+              </div>
             </div>
           </div>
         </div>
         <div className="third-container">
-            <h1>Top Events</h1>
-            <div>
-                <img src={img4}></img>
-                <h2>Education</h2>
-                 <div>
-                 Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall. Gåsade
-                döloning. Syll bebel <br></br>till kvasisens. Tres kos masade{" "}
-                <br></br>robotdräkt. Heteropp. Vönäna teraskap.<br></br> Vande
-                legga kadinera makrohet <br></br>reatologi. Por hybridkrig och
-                hypobun.<br></br> Evöligt färade, därför att påns tilig.{" "}
-                 </div>
-
+          <div className="third-contant">
+            <div className="third-top">
+              <h1>Top Events</h1>
             </div>
-            <div>
+            <div className="third-full-con">
+              <div className="third-sub-con">
+                <div className="third-sub-con">
+                  <img src={img4}></img>
+                </div>
+                <div className="third-sub-top">
+                  <h2>Education</h2>
+                </div>
+
+                <p className="sub-contain">
+                  {" "}
+                  Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall.
+                  Gåsade döloning. Syll bebel <br></br>till kvasisens. Tres kos
+                  masade <br></br>robotdräkt. Heteropp. Vönäna teraskap.
+                  <br></br> Vande legga kadinera makrohet <br></br>reatologi.
+                  Por hybridkrig och hypobun.<br></br> Evöligt färade, därför
+                  att påns tilig.{" "}
+                </p>
+              </div>
+              <div className="third-sub-con">
                 <img src={img4}></img>
-                <h2>Education</h2>
-                 <div>
-                 Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall. Gåsade
-                döloning. Syll bebel <br></br>till kvasisens. Tres kos masade{" "}
-                <br></br>robotdräkt. Heteropp. Vönäna teraskap.<br></br> Vande
-                legga kadinera makrohet <br></br>reatologi. Por hybridkrig och
-                hypobun.<br></br> Evöligt färade, därför att påns tilig.{" "}
-                 </div>
 
-            </div>
-            <div>
+                <div className="third-sub-top">
+                  <h2>Education</h2>
+                </div>
+
+                <p className="sub-contain">
+                  {" "}
+                  Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall.
+                  Gåsade döloning. Syll bebel <br></br>till kvasisens. Tres kos
+                  masade <br></br>robotdräkt. Heteropp. Vönäna teraskap.
+                  <br></br> Vande legga kadinera makrohet <br></br>reatologi.
+                  Por hybridkrig och hypobun.<br></br> Evöligt färade, därför
+                  att påns tilig.{" "}
+                </p>
+              </div>
+              <div className="third-sub-con">
                 <img src={img4}></img>
-                <h2>Education</h2>
-                 <div>
-                 Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall. Gåsade
-                döloning. Syll bebel <br></br>till kvasisens. Tres kos masade{" "}
-                <br></br>robotdräkt. Heteropp. Vönäna teraskap.<br></br> Vande
-                legga kadinera makrohet <br></br>reatologi. Por hybridkrig och
-                hypobun.<br></br> Evöligt färade, därför att påns tilig.{" "}
-                 </div>
 
+                <div className="third-sub-top">
+                  <h2>Education</h2>
+                </div>
+
+                <p className="sub-contain">
+                  {" "}
+                  Lörem ipsum kvasitropi svemester.<br></br> Anaitet lall.
+                  Gåsade döloning. Syll bebel <br></br>till kvasisens. Tres kos
+                  masade <br></br>robotdräkt. Heteropp. Vönäna teraskap.
+                  <br></br> Vande legga kadinera makrohet <br></br>reatologi.
+                  Por hybridkrig och hypobun.<br></br> Evöligt färade, därför
+                  att påns tilig.{" "}
+                </p>
+              </div>
             </div>
-
-
-
+          </div>
         </div>
         <div>
-        <Footer />
+          <Footer />
         </div>
-
       </>
     </div>
   );
