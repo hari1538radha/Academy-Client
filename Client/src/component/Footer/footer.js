c72efaad191de477f887411f4f0dd671ec815940
SRIRAM-D-B <sriramdevarajulu@gmail.com>
2022-09-27 15:53:34 +0530

    responsive footer for desktop and tab -sriram

--- Client/src/component/Footer/footer.js
+++ Client/src/component/Footer/footer.js
@@ -12,56 +12,51 @@ import instagram from "../Login/Images/Instagram.svg";
 import google from "../Login/Images/GooglePlus.svg";
 import pinster from "../Login/Images/Pinterest.svg";
 import Wifilogo from "../Login/Images/wifilogo.svg";
-import './footer.css'
+import "./footer.css";
 
 const footer = () => {
   return (
     <div className="footer">
-      <div className="Linepic">
-        <img src={Linepic}></img>
-      </div>
-
-      <img className="Logo" src={Logo}></img>
+      <div className="line"></div>
+      <img className="logo" src={Logo}></img>
       <div>
-        <div className="Location-footer">
+        <div className="location-footer">
           {" "}
           <img src={Location}></img>
           <p>345 Faulconer Drive, Suite 4 • Charlottesville</p>
         </div>
-        <div className="Caller-container">
-          <img src={callerlogo}></img>
-          <p>(123) 456-7</p>
-        </div>
-        <div className="printer-container">
-          <img src={printerlogo}></img>
-          <p>(123) 456-7</p>
-        </div>
-        <div className="Social">
-          <p>Social Media</p>
-          <div className="Social-logos">
-            <img src={facebook} alt="facebook"></img>
-            <img src={twitter} alt="twitter"></img>
-            <img src={Linkedin} alt="Linkedin"></img>
-            <img src={youtube} alt="Youtube"></img>
-            <img src={instagram} alt="Instagram"></img>
-            <img src={google} alt="Google"></img>
-            <img src={pinster} alt="Pinster"></img>
-            <img src={Wifilogo} alt="wifilogo"></img>
+        <div className="contact-container">
+          <div className="caller-container">
+            <img src={callerlogo}></img>
+            <p>(123) 456-7</p>
+          </div>
+          <div className="printer-container">
+            <img src={printerlogo}></img>
+            <p>(123) 456-7</p>
           </div>
         </div>
+        <div className="social-container">
+          <span className="para">Social Media</span>
+          <img src={facebook} alt="facebook"></img>
+          <img src={twitter} alt="twitter"></img>
+          <img src={Linkedin} alt="Linkedin"></img>
+          <img src={youtube} alt="Youtube"></img>
+          <img src={instagram} alt="Instagram"></img>
+          <img src={google} alt="Google"></img>
+          <img src={pinster} alt="Pinster"></img>
+          <img src={Wifilogo} alt="wifilogo"></img>
+        </div>
       </div>
-      <div className="Line2"></div>
-      <div className="footer-link-container">
-        <div className="Footer-link">
-          <a href="#">ABOUT US</a>
-          <a href="#"> CONTACT US </a>
-          <a href="#">HELP</a>
-          <a href="#">PRIVACY POLICY</a>
-          <a href="#"> DISCLAIMER</a>
-        </div>
-        <div className="Copyright-container">
-          <p>Copyright © 2018 • Lift Media Inc.</p>
-        </div>
+      <div className="line2"></div>
+      <div className="footer-link">
+        <a href="#">ABOUT US</a>
+        <a href="#"> CONTACT US </a>
+        <a href="#">HELP</a>
+        <a href="#">PRIVACY POLICY</a>
+        <a href="#"> DISCLAIMER</a>
+      </div>
+      <div className="Copyright-container">
+        <p>Copyright © 2018 • Lift Media Inc.</p>
       </div>
     </div>
   );
