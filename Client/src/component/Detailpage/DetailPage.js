23424b4d1e9fddf5feac66a1bd3fb99d2c92123c
DHARUNDP5 <dharunprakashkdp@gmail.com>
2022-10-10 09:19:56 +0530

    detail page responssive added

--- Client/src/component/Detailpage/DetailPage.js
+++ Client/src/component/Detailpage/DetailPage.js
@@ -17,39 +17,43 @@ function DetailPage() {
   // }))
   return (
     <div>
-      <div className="header-container">
-        <h1 className="title">Java</h1>
-        <p className="content-container">
-          Sos pipoda bärar. Spegt pseudon dorade. Töngar podyda potinat. Ove
-          Holm niled. Multirade lädirade lell. Kode jäkiren, Lina Bengtsson.
-          Robothandel pevis: och al.lorem ipsum dipse ulti lorem iupsumoch al.lorem ipsum dipse ulti lorem iupsum
-        </p>
-        <img className="detailimage" src={courseimage} />
-        <p className="subcontent-container">
-          Sos pipoda bärar. Spegt pseudon dorade. Töngar podyda potinat. Ove
-          Holm niled. Multirade lädirade lell. Kode jäkiren, Lina Bengtsson.
-          Robothandel pevis: och al.lorem ipsum dipse ulti lorem iupsum Sos
-          pipoda bärar. Spegt pseudon dorade. Töngar podyda potinat. Ove Holm
-          niled. Multirade lädirade lell. Kode jäkiren, Lina Bengtsson.
-          Robothandel pevis: och al.lorem ipsum dipse ulti lorem iupsum Sos
-          pipoda bärar. Spegt pseudon dorade. Töngar podyda potinat. Ove Holm
-
+      <div className="navbar-container" >
+      </div>
+      <div className="mainbody-container">
+        <div className="header-container">
+          <h1 className="title">Java</h1>
+          <p className="content-container">
+            Sos pipoda bärar. Spegt pseudon dorade. Töngar podyda potinat. Ove
+            Holm niled. Multirade lädirade lell. Kode jäkiren, Lina Bengtsson.
+            Robothandel pevis: och al.lorem ipsum dipse ulti lorem iupsumoch al.lorem ipsum dipse ulti lorem iupsum
+          </p>
+          <img className="detailimage" src={courseimage} />
+          <p className="subcontent-container">
+            Sos pipoda bärar. Spegt pseudon dorade. Töngar podyda potinat. Ove
+            Holm niled. Multirade lädirade lell. Kode jäkiren, Lina Bengtsson.
+            Robothandel pevis: och al.lorem ipsum dipse ulti lorem iupsum Sos
+            pipoda bärar. Spegt pseudon dorade. Töngar podyda potinat. Ove Holm
+            niled. Multirade lädirade lell. Kode jäkiren, Lina Bengtsson.
+            Robothandel pevis: och al.lorem ipsum dipse ulti lorem iupsum Sos
+            pipoda bärar. Spegt pseudon dorade. Töngar podyda potinat. Ove Holm
+          </p>
           <a className="see-more">See more</a>
-        </p>
         </div>
-        
+
         <div className="course-container">
-          <p className="course-title"><a href="">Chapters</a></p>
-          <p className="course-title"><a href="">Machines</a></p>
-          <p className="course-title"><a href="">Electrical</a></p>
-          <p className="course-title"><a href="">Java</a></p>
-          <p className="course-title"><a href="">C++</a></p>
-          <p className="course-title"><a href="">Python</a></p>
-          <p className="course-title"><a  href="">Javascript</a></p>
+          <p><a href="">Chapters</a></p>
+          <p><a href="">Machines</a></p>
+          <p><a href="">Electrical</a></p>
+          <p><a href="">Java</a></p>
+          <p><a href="">C++</a></p>
+          <p><a href="">Python</a></p>
+          <p><a href="">Javascript</a></p>
           <p><a className="course-seemore-btn " href="">SeeMore</a>
           </p>
         </div>
-        </div> 
+      </div>
+
+    </div>
 
   );
 }
