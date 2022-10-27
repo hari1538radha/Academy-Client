4e6332cfa16a007b4ec2f3b56114ef7ea3346d68
versatilemage <104298679+versatilemage@users.noreply.github.com>
2022-10-27 10:13:30 +0530

    fresh backend

--- Server/server.js
+++ Server/server.js
@@ -11,9 +11,9 @@ const app = Express();
 
 const PORT = process.env.PORT || 8001;
 
-app.use(cors({ credentials: true, origin:"http://localhost:3000"}));
+app.use(cors({ credentials: true, origin:"https://learnplusplus.vercel.app"}));
 
-// "https://learnplusplus.vercel.app"
+// "http://localhost:3000"
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({limit: '50mb', extended: true}));
